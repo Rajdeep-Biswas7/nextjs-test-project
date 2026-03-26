a4c56fcebe00d85b07be41d15c80895ebecf803a
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-03-26 23:43:19 +0530

    updated page and testFile

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { GlowCard } from "@/components/glow-card";
+import TestFile from "@/components/testFile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-4 bg-neutral-600">
       Home
+      <TestFile />
       <GlowCard className="w-80 h-100">Its the glowcard.</GlowCard>
     </div>
   );
---
+++ components/testFile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function testFile() {
+  return (
+    <div>testFile</div>
+  )
+}
+
+export default testFile
